72deed71b1db94d3dff4ee961c122f0223d632a2
PARIS <alejoriios14@gmail.com>
2026-03-20 15:24:48 -0500

    Fountation

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "iBuiltThis",
+  description: "iBuiltThis is a platform for building and sharing your own projects with the world.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${outfit.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="">Hello Next.js</h1>
+      
+    </div>
+  );
+}
