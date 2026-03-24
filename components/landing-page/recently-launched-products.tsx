5cb6fbea96e0a5b8c4ad6e9e839cd0bd660bc44d
PARIS <alejoriios14@gmail.com>
2026-03-24 08:20:23 -0500

    Feat: Recently Launched Section

---
+++ components/landing-page/recently-launched-products.tsx
@@ -0,0 +1,36 @@
+import { CalendarIcon, RocketIcon } from "lucide-react";
+import SectionHeader from "../common/section-header";
+import ProductCard from "../products/ProductCard";
+import EmptyState from "../common/empty-state";
+
+
+const recentlyLaunchedProducts:any = []
+
+export default function RecentlyLaunchedProducts() {
+    return (
+        <section className="py-20">
+            <div className="wrapper ">
+                <SectionHeader
+                    title="Recently Launched"
+                    icon={RocketIcon}
+                    description="Discover the lastest products from our community"
+                />
+
+                {recentlyLaunchedProducts.length > 0 ? (
+                    <div className="grid-wrapper">
+                        {recentlyLaunchedProducts.map((product:any) => (
+                            <ProductCard key={product.id} {...product} />
+                        ))}
+                    </div>
+                ) : (
+                    <EmptyState
+                        message="No product launched in the last week. Check back soon for new launches"
+                        icon={CalendarIcon}
+                    />
+                )}
+
+            </div>
+        </section>
+    )
+
+}
